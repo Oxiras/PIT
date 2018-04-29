8b015a994995b84fcef48faeee0f085acc6321a3
hieutran <trunghieu.tran@utdallas.edu>
2018-04-28 23:57:24 -0500

    Add program method-coverage

---
+++ method-coverage/method-coverage/src/main/java/edu/utd/methodcoverage/Agent.java
@@ -0,0 +1,54 @@
+package edu.utd.methodcoverage;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+
+public class Agent {
+
+	public static void premain(String agentArgs, Instrumentation inst) {
+		inst.addTransformer(new ClassFileTransformer() {
+			@Override
+			public byte[] transform(ClassLoader classLoader, String s, Class<?> aClass,
+					ProtectionDomain protectionDomain, byte[] bytes) throws IllegalClassFormatException {
+				if (!isNotSystemRunning(s)) {
+					System.out.println(s);
+					try {
+						ClassReader cr = new ClassReader(bytes);
+						System.out.println(cr == null);
+						ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+						// Visit all classes and "instrument"
+						ClassCollector ca = new ClassCollector(cw);
+						cr.accept(ca, 0);
+						return cw.toByteArray();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				return null;
+			}
+
+			/**
+			 * filter some system files and tool file (eg. maven, junit)
+			 * 
+			 * @param s
+			 *            class file path
+			 * @return
+			 */
+			private boolean isNotSystemRunning(String s) {
+				if (s.contains("java/") 
+						|| s.contains("sun/") 
+						|| s.contains("maven/") 
+//						|| s.contains("junit/")
+						|| s.contains("phosphor/"))
+					return true;
+				return false;
+			}
+		}, true);
+	}
+
+}
---
+++ method-coverage/method-coverage/src/main/java/edu/utd/methodcoverage/MethodCollector.java
@@ -0,0 +1,35 @@
+package edu.utd.methodcoverage;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+class MethodCollector extends MethodVisitor implements Opcodes {
+	String mName;
+
+	public MethodCollector(final MethodVisitor mv, String name) {
+		super(ASM5, mv);
+		this.mName = name;
+	}
+	/**
+	 * Collect method information by printing method name when it runs
+	 */
+	@Override
+	public void visitCode() {
+		if (mName.contains("junit.framework.AssertionFailedError")) {
+			print("===");
+		}
+		print(mName);
+		/*
+		 * Write method name to file, but I dont know why it dose not work in the large number of methods
+		 * So I have just print methods' name to console and cat them to file, then do post process
+		*/
+		
+		super.visitCode();
+	}
+	private void print(String content) {
+		mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+		mv.visitLdcInsn(content);
+		mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+	}
+
+}
